89c528db5d62c3acbfc69120989b35ddc8d81e9a
Ecyo Felipe <felipe_icb@hotmail.com>
2023-05-19 16:14:37 -0300

    siteReformado

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// No início do arquivo "script.js"
+ocultarExtensaoHTML();
+
+
 const inputs = document.querySelectorAll('.input');
 const button = document.querySelector('.login__button');
 
@@ -27,33 +31,85 @@ inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
 inputs.forEach((input) => input.addEventListener('input', handleChange));
 
-document.addEventListener('DOMContentLoaded', function() {
-  var loginButton = document.querySelector('.login__button');
-  loginButton.addEventListener('click', checkPassword);
+// Função para verificar o login e redirecionar o usuário
+function fazerLogin() {
+  const senha = document.querySelector('input[name="password"]').value;
+  const manterLogin = document.querySelector('.input--checkbox').checked;
 
-  // Verificar se o usuário já está logado
-  var isLoggedIn = localStorage.getItem('isLoggedIn');
-  var checkboxInput = document.querySelector('.input--checkbox');
-  checkboxInput.checked = isLoggedIn === 'true';
-});
-
-function checkPassword() {
-  var passwordInput = document.querySelector('input[name="password"]');
-  var password = passwordInput.value;
-
-  if (password === 'kit1pc2023') {
-    var checkboxInput = document.querySelector('.input--checkbox');
-    if (checkboxInput.checked) {
-      // Armazenar a informação de login no localStorage
-      localStorage.setItem('isLoggedIn', 'true');
-    }
-
-    if (window.location.href.endsWith(".html")) {
-      window.location.href = window.location.href.replace(".html", "");
-    } else {
-      window.open('./jogos.html', '_blank');
-    }
-  } else {
-    alert('Senha incorreta, tente novamente.')
+  if (senha === 'kit1pc2023' && manterLogin) {
+    // Salvar a senha do usuário no computador (exemplo usando localStorage)
+    localStorage.setItem('senhaUsuario', senha);
+    // Redirecionar para outra página
+    window.location.href = 'jogos.html';
   }
 }
+
+// Função para ocultar a extensão .html nas páginas
+function ocultarExtensaoHTML() {
+  const paginas = document.querySelectorAll('a[href$=".html"]');
+  paginas.forEach((pagina) => {
+    const href = pagina.getAttribute('href');
+    const novaHref = href.replace('.html', '');
+    pagina.setAttribute('href', novaHref);
+  });
+}
+
+
+// No final do arquivo "script.js"
+const buttonEntrar = document.querySelector('.login__button');
+buttonEntrar.addEventListener('click', fazerLogin);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var loginButton = document.querySelector('.login__button');
+//   loginButton.addEventListener('click', checkPassword);
+
+//   // Verificar se o usuário já está logado
+//   var isLoggedIn = localStorage.getItem('isLoggedIn');
+//   var checkboxInput = document.querySelector('.input--checkbox');
+//   checkboxInput.checked = isLoggedIn === 'true';
+// });
+
+// function checkPassword() {
+//   var passwordInput = document.querySelector('input[name="password"]');
+//   var password = passwordInput.value;
+
+//   if (password === 'kit1pc2023') {
+//     var checkboxInput = document.querySelector('.input--checkbox');
+//     if (checkboxInput.checked) {
+//       // Armazenar a informação de login no localStorage
+//       localStorage.setItem('isLoggedIn', 'true');
+//     }
+
+//     if (window.location.href.endsWith(".html")) {
+//       window.location.href = window.location.href.replace(".html", "");
+//     } else {
+//       window.open('./jogos.html', '_blank');
+//     }
+//   } else {
+//     alert('Senha incorreta, tente novamente.')
+//   }
+// }
